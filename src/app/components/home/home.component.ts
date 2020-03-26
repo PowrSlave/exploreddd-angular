b398c9cadaa725932d492008921dfdc0ad24c802
Zachariah Michaelonious Aquinas <zach.johnston@gmail.com>
2020-03-26 10:07:01 -0600

    testing conversion of jquery method initialization

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-home',
@@ -11,6 +12,21 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     //homepage resize jquery/javascript likely should go here?
+    $(document).ready(function() {
+      //console.log('hio! you hit the homepage module!');
+      $(window).resize(function() {
+        var bodyheight = $(this).height();
+        var navheight = $('.navbar').outerHeight(true);
+        var homepagetopbar = $('.homepage--top-bar').outerHeight(true);
+        $("#sidebar").height(bodyheight);
+        // console.log('height of window is ' + bodyheight);
+        // console.log('height of nav div is ' + navheight);
+        // console.log('height of homepage top bar div is ' + homepagetopbar);
+        var videoheight = bodyheight - navheight - homepagetopbar;
+        $(".homepage--hero-video-container").css("margin-top",homepagetopbar);
+        $(".homepage--hero-video-container").css("height",videoheight);
+      }).resize();
+    });
   }
 
 }
