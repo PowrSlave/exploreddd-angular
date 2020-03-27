a187709529ffb42cf8b856b0ecaeb3282e526ec7
Zachariah Michaelonious Aquinas <zach.johnston@gmail.com>
2020-03-27 09:32:24 -0600

    added some basic error handling to data service

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,21 @@ export class DataService {
 
   constructor(private httpClient: HttpClient) { }
 
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Unknown error!';
+    if (error.error instanceof ErrorEvent) {
+      // Client-side errors
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // Server-side errors
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
+
   sendGetRequest() {
-    return this.httpClient.get(this.apiUrl);
+    return this.httpClient.get(this.apiUrl).pipe(catchError(this.handleError));
   }
 
 }
