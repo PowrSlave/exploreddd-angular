4db3153499db8a1f9d5e772d7c01fc12d60b3a7b
Zachariah Michaelonious Aquinas <zach.johnston@gmail.com>
2020-04-06 10:45:16 -0600

    added dataService to workshops page

--- src/app/components/workshops/workshops.component.ts
+++ src/app/components/workshops/workshops.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-workshops',
   templateUrl: './workshops.component.html',
-  styleUrls: ['./workshops.component.scss']
+  styleUrls: ['./workshops.component.scss'],
+  providers: [DataService]
 })
-export class WorkshopsComponent implements OnInit {
 
-  constructor() { }
+export class WorkshopsComponent implements OnInit, OnDestroy {
+
+  speakers:Array<any>;
+  destroy$: Subject<boolean> = new Subject<boolean>();
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+
+    this.dataService.getSpeakers().pipe(takeUntil(this.destroy$)).subscribe((data: any[])=>{
+      console.log(data);
+      this.speakers = data;
+    });
+
+  }
+
+
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    // Unsubscribe from the subject
+    this.destroy$.unsubscribe();
   }
 
 }
