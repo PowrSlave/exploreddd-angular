38bba019ea128fa6e0ae52f604d7dddd58cef707
Zachariah Michaelonious Aquinas <zach.johnston@gmail.com>
2020-04-10 16:39:23 -0600

    fixed home link in archived 2019 index page

--- src/app/components/schedule/schedule.component.ts
+++ src/app/components/schedule/schedule.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, Renderer2, ElementRef } from '@angular/core';
 import { takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
 import { DataService } from '../../services/data.service';
@@ -15,14 +15,39 @@ export class ScheduleComponent implements OnInit, OnDestroy {
   destroy$: Subject<boolean> = new Subject<boolean>();
   scheduleBlob:String;
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService,
+              private renderer: Renderer2,
+              private el: ElementRef) { }
 
   ngOnInit() {
-    this.dataService.getSchedule().pipe(takeUntil(this.destroy$)).subscribe((data: any[])=>{
-      console.log(data);
-      this.scheduleBlob = JSON.stringify(data);
-    })
+
+    //if we go the json route
+    // this.dataService.getSchedule().pipe(takeUntil(this.destroy$)).subscribe((data: any[])=>{
+    //   console.log(data);
+    //   this.scheduleBlob = JSON.stringify(data);
+    // })
+
+
+    //if we go the styled and embedded script tag route
+    /*
+    Utilizing this technique (http://blog.davidjs.com/2018/09/insert-script-tags-in-angular-components/)
+    to embed a script tag in the component template
+    */
+
+    const s = this.renderer.createElement('script');
+    s.type = 'text/javascript';
+    s.src = 'https://sessionize.com/api/v2/lrqa96hc/view/GridSmart';
+    s.text = ``;
+    this.renderer.appendChild(this.el.nativeElement, s);
+
+    // const div = this.renderer.createElement('div');
+    // const text = this.renderer.createText('Hello world!');
+
+    // this.renderer.appendChild(div, text);
+    // this.renderer.appendChild(this.el.nativeElement, div);
+
   }
+
   ngOnDestroy() {
     this.destroy$.next(true);
     // Unsubscribe from the subject
