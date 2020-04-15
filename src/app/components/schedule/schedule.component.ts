d99ef0b8c4d5063d74130063e77f8c5d16cdfc55
Zachariah Michaelonious Aquinas <zach.johnston@gmail.com>
2020-04-15 14:10:21 -0600

    sessionize schedule polished. 80% through the tunnel

--- src/app/components/schedule/schedule.component.ts
+++ src/app/components/schedule/schedule.component.ts
@@ -1,22 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
+declare var jQuery: any;
 
 @Component({
   selector: 'app-schedule',
   templateUrl: './schedule.component.html',
   styleUrls: ['./schedule.component.scss']
 })
-export class ScheduleComponent implements OnInit {
+export class ScheduleComponent implements OnInit, OnDestroy {
 
   name = 'Set iframe source';
-  //url: string = "https://angular.io/api/router/RouterLink";
   url: string = "schedule-page/sessionizeSchedule.html";
   urlSafe: SafeResourceUrl;
 
   constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
+
+    //trust the async loaded script pulled in via a static page nested in an iframe *sheesh*
     this.urlSafe= this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
+
+    const myIframe = document.getElementById("myIframe");
+      myIframe.onload = () => {
+        var myTimeout = setInterval(function() {
+          const dynamicElement = myIframe.contentWindow.document.getElementById("sessionize");
+          if (myIframe.contentWindow.document.body.contains(dynamicElement)) {
+            console.log('sessionize has emerged!');
+            console.log(dynamicElement);
+            //console.log(myIframe.contentWindow.document.getElementById("sessionize").offsetHeight);
+            // let myIframeHeight = myIframe.contentWindow.document.body.clientHeight+'px';
+            console.log( jQuery('#myIframe').contents().height() + ' is the height' );
+            let myIframeHeight = jQuery('#myIframe').contents().height() + 500 + 'px'; //adding 500? eww.
+            //console.log(myIframeHeight);
+            document.getElementById("myIframe").style.height = myIframeHeight;
+            clearInterval(myTimeout);
+          }
+        }, 100);
+      }
+  }
+
+  ngOnDestroy() {
+
   }
 
 }
