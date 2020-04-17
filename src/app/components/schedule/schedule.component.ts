93dfba14dc5763741820b92dd758db802bf4fe1f
Zachariah Michaelonious Aquinas <zach.johnston@gmail.com>
2020-04-17 11:20:40 -0600

    removed iframe code from schedule page

--- src/app/components/schedule/schedule.component.ts
+++ src/app/components/schedule/schedule.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-
-declare var jQuery: any;
 
 @Component({
   selector: 'app-schedule',
@@ -10,32 +7,10 @@ declare var jQuery: any;
 })
 export class ScheduleComponent implements OnInit, OnDestroy {
 
-  name = 'Set iframe source';
-  url: string = "schedule-page/sessionizeSchedule.html";
-  urlSafe: SafeResourceUrl;
-
-  constructor(private sanitizer: DomSanitizer) { }
+  constructor() { }
 
   ngOnInit() {
 
-    //trust the async loaded script pulled in via a static page nested in an iframe *sheesh*
-    this.urlSafe= this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
-
-    const myIframe = <HTMLIFrameElement>document.getElementById("myIframe");
-      myIframe.onload = () => {
-        var myTimeout = setInterval(function() {
-          const dynamicElement = myIframe.contentWindow.document.getElementById("sessionize");
-          if (myIframe.contentWindow.document.body.contains(dynamicElement)) {
-            console.log('sessionize has emerged!');
-            console.log(dynamicElement);
-            console.log( jQuery('#myIframe').contents().height() + ' is the height' );
-            let myIframeHeight = jQuery('#myIframe').contents().height() + 500 + 'px'; //adding 500? eww.
-            //console.log(myIframeHeight);
-            document.getElementById("myIframe").style.height = myIframeHeight;
-            clearInterval(myTimeout);
-          }
-        }, 100);
-      }
   }
 
   ngOnDestroy() {
