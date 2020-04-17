a315bf03e99ad79915ee985a31e61ac896e2d40d
Zachariah Michaelonious Aquinas <zach.johnston@gmail.com>
2020-04-17 11:49:38 -0600

    schedule component data foundation is setup

--- src/app/components/schedule/schedule.component.ts
+++ src/app/components/schedule/schedule.component.ts
@@ -1,20 +1,33 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { Config } from '../../services/data.service';
 
 @Component({
   selector: 'app-schedule',
   templateUrl: './schedule.component.html',
-  styleUrls: ['./schedule.component.scss']
+  styleUrls: ['./schedule.component.scss'],
+  providers: [DataService]
 })
+
 export class ScheduleComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+
+  destroy$: Subject<boolean> = new Subject<boolean>();
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
-
+    this.dataService.getAllData().pipe(takeUntil(this.destroy$)).subscribe((data: Config)=>{
+      console.log(data);
+    });
   }
 
   ngOnDestroy() {
-
+    this.destroy$.next(true);
+    // Unsubscribe from the subject
+    this.destroy$.unsubscribe();
   }
 
 }
